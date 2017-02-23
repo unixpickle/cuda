5f41c41e0437906dc36a811d1dfe0846aa32878a
Alex Nichol <unixpickle@gmail.com>
2017-02-22 20:08:14 -0500

    implemented Context API
    This API is like the private loop API in anyvec, i.e.:
    
    https://github.com/unixpickle/anyvec/blob/883a8817c4baeb12a26cff317dba7ba4fe350ebd/cuda/loop.go
    
    However, the system here is a lot simpler. Whether or not this works well has yet to be determined, but I am optimistic about the new API.

---
+++ context.go
@@ -0,0 +1,73 @@
+package cuda
+
+/*
+#include "cuda.h"
+*/
+import "C"
+import "runtime"
+
+const defaultContextBuffer = 10
+
+// A Context maintains a CUDA-dedicated thread.
+// All CUDA code should be run by a Context.
+type Context struct {
+	msgs chan<- *contextMsg
+}
+
+// NewContext creates a new Context.
+//
+// The bufferSize is the maximum number of asynchronous
+// calls that can be queued up at once.
+// A bufferSize of -1 is replaced with a reasonable
+// default.
+func NewContext(bufferSize int) (*Context, error) {
+	if bufferSize < -1 {
+		panic("buffer size out of range")
+	} else if bufferSize == -1 {
+		bufferSize = defaultContextBuffer
+	}
+	msgs := make(chan *contextMsg, bufferSize)
+	go contextLoop(msgs)
+	res := &Context{msgs: msgs}
+	err := <-res.Run(func() error {
+		return NewErrorDriver("cuInit", C.cuInit(0))
+	})
+	if err != nil {
+		close(msgs)
+		return nil, err
+	}
+	runtime.SetFinalizer(res, func(obj interface{}) {
+		close(obj.(*Context).msgs)
+	})
+	return res, nil
+}
+
+// Run runs f in the Context asynchronously and returns
+// a channel that will be sent the result of f.
+//
+// While f is running, no other function can run on the
+// Context.
+// This means that, to avoid deadlock, f should not use
+// the Context in a blocking manner.
+func (c *Context) Run(f func() error) <-chan error {
+	ch := make(chan error, 1)
+	msg := &contextMsg{
+		f:        f,
+		doneChan: ch,
+	}
+	c.msgs <- msg
+	return ch
+}
+
+type contextMsg struct {
+	f        func() error
+	doneChan chan<- error
+}
+
+func contextLoop(msgs <-chan *contextMsg) {
+	runtime.LockOSThread()
+	for msg := range msgs {
+		msg.doneChan <- msg.f()
+		close(msg.doneChan)
+	}
+}
